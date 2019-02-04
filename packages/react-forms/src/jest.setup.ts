b7797e131d370606151fa5597d0ea547b66948f8
John Freeman <jfreeman08@gmail.com>
2019-02-03 20:16:14 -0600

    Use default test configuration

--- packages/react-forms/src/jest.setup.ts
+++
@@ -1,4 +0,0 @@
-// These must be imports, not requires. Why?
-// https://github.com/kentcdodds/react-testing-library#global-config
-import 'jest-dom/extend-expect'
-import 'react-testing-library/cleanup-after-each'
