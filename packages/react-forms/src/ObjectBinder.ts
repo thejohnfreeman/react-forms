013973389ac95fcf6df77e28db1441b5f4cbe7e9
John Freeman <jfreeman08@gmail.com>
2019-02-10 09:26:50 -0600

    ObjectBinder should render null instead of undefined

--- packages/react-forms/src/ObjectBinder.ts
+++ packages/react-forms/src/ObjectBinder.ts
@@ -6,16 +6,16 @@ import { ShouldBe } from './Binder'
 // identifier).
 export class ObjectBinder<
   O extends object = { text: string; value: number }
-> extends AbstractBinder<O, O | undefined> {
+> extends AbstractBinder<O, O | null> {
   public constructor(defaultValue: O | null = null) {
     super('object', defaultValue)
   }
 
-  public parse(repr: O | undefined): ShouldBe<O | null> {
-    return { value: repr || null }
+  public parse(repr: O | null): ShouldBe<O | null> {
+    return { value: repr }
   }
 
-  public render(value: O | null): O | undefined {
-    return value || undefined
+  public render(value: O | null): O | null {
+    return value
   }
 }
