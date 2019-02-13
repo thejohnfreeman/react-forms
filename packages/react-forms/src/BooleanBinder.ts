dd7562ff71cbdf40536160ed603364c99b9f5d23
John Freeman <jfreeman08@gmail.com>
2019-02-12 22:30:34 -0600

    Make Boolean view models optional by default

--- packages/react-forms/src/BooleanBinder.ts
+++ packages/react-forms/src/BooleanBinder.ts
@@ -3,5 +3,6 @@ import { AbstractBinder } from './AbstractBinder'
 export class BooleanBinder extends AbstractBinder<boolean, boolean> {
   public constructor(defaultValue: boolean = false) {
     super('boolean', defaultValue)
+    this.optRequired = false
   }
 }
