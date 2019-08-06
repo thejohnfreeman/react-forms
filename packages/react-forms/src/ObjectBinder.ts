a411134732b8bfadaf78d015a953b4871d45ee40
John Freeman <jfreeman08@gmail.com>
2019-08-06 10:54:22 -0500

    Translate object shapes

--- packages/react-forms/src/ObjectBinder.ts
+++ packages/react-forms/src/ObjectBinder.ts
@@ -1,12 +1,43 @@
-import { IdentityOptionalBinder } from './IdentityOptionalBinder'
+import { ShouldBe } from './Binder'
+import { AbstractOptionalBinder } from './AbstractOptionalBinder'
 
 // A binder for objects selected from a list. They'll generally have a name
 // (or other human-readable representation) and a value (usually an
 // identifier).
 export class ObjectBinder<
-  O extends object = { text: string; value: number }
-> extends IdentityOptionalBinder<O> {
-  public constructor(defaultValue: O | null = null) {
+  V extends object = { text: string; value: number },
+  R extends object = V
+> extends AbstractOptionalBinder<V, R> {
+  public optShape: Record<string, string> = { text: 'text', value: 'value' }
+
+  public constructor(defaultValue: V | null = null) {
     super('object', defaultValue)
+  }
+
+  public to(shape: Record<string, string>): this {
+    this.optShape = shape
+    return this
+  }
+
+  public parse(repr: R | undefined): ShouldBe<V | null> {
+    if (typeof repr === 'undefined') {
+      return { value: null }
+    }
+    const value = {} as V
+    for (const [rkey, vkey] of Object.entries(this.optShape)) {
+      value[vkey] = repr[rkey]
+    }
+    return { value }
+  }
+
+  public render(value: V | null): R | undefined {
+    if (value === null) {
+      return
+    }
+    const repr = {} as R
+    for (const [rkey, vkey] of Object.entries(this.optShape)) {
+      repr[rkey] = value[vkey]
+    }
+    return repr
   }
 }
